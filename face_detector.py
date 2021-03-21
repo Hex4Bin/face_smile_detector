5af972b7bb9d52430fc71a5006653af1bb914f1b
Hex4Bin <hex4bin@protonmail.ch>
2021-03-21 20:07:38 +0100

    new parameters for detection

--- face_detector.py
+++ face_detector.py
@@ -1,34 +1,12 @@
 from cv2 import cv2
 from random import randrange
 
-"""
-# load some pre-trained frontal face data from opencv
-trained_face_data = cv2.CascadeClassifier(
-    'haarcascade_frontalface_default.xml')
 
-# load an image and convert it to grayscaled version for test
-img = cv2.imread('2faces.jpg')
-#img = cv2.imread('rdj.jpg')
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
-# the coordinates come this format: [[100 115 225 225]]
-# we have to make a tuple based on the data
-for (x, y, w, h) in face_coordinates:
-    # draw rectangles around the faces with some randomly generated colors
-    cv2.rectangle(img, (x, y), (x + w, y + h),
-                  (randrange(128, 256), randrange(128, 256), randrange(128, 256)), 5)
-
-# display the image
-cv2.imshow('Test', img)
-cv2.waitKey()
-"""
+# Our pre-trained face classifier
+classifier_face = './haarcascade_frontalface_default.xml'
 
 # load some pre-trained frontal face data from opencv
-trained_face_data = cv2.CascadeClassifier(
-    'haarcascade_frontalface_default.xml')
+face_tracker = cv2.CascadeClassifier(classifier_face)
 
 # capture video from webcam
 # 0 means the default webcam
@@ -43,7 +21,8 @@ while True:
     grayscaled_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detect faces
-    face_coordinates = trained_face_data.detectMultiScale(grayscaled_video)
+    face_coordinates = face_tracker.detectMultiScale(
+        image=grayscaled_video, scaleFactor=1.2, minNeighbors=6, minSize=(120, 120))
 
     # the coordinates come this format: [[100 115 225 225]]
     # we have to make a tuple based on the data
@@ -55,9 +34,10 @@ while True:
     cv2.imshow('PRESS ESC TO QUIT', frame)
     key = cv2.waitKey(1)
 
-    # stop if "q" is pressed
+    # stop if "ESC" is pressed
     if key == 27:
         break
 
 # release the VideoCapture object
 webcam.release()
+cv2.destroyAllWindows()
--- face_detector_pic.py
+++ face_detector_pic.py
@@ -1,18 +1,19 @@
 from cv2 import cv2
 from random import randrange
 
-
-classifier_face = './haarcascade_frontalface_default.xml'
+# Our pre-trained face classifier
+CLASSIFIER_FACE = './haarcascade_frontalface_default.xml'
 
 # load some pre-trained frontal face data from opencv
-face_tracker = cv2.CascadeClassifier(classifier_face)
+face_tracker = cv2.CascadeClassifier(CLASSIFIER_FACE)
 
 # load an image and convert it to grayscaled version for test
 img = cv2.imread('2faces.jpg')
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # detect faces
-face_coordinates = face_tracker.detectMultiScale(grayscaled_img)
+face_coordinates = face_tracker.detectMultiScale(
+    image=grayscaled_img, scaleFactor=1.2, minNeighbors=6, minSize=(120, 120))
 
 # the coordinates come this format: [[100 115 225 225]]
 # we have to make a tuple based on the data
